9ae66d5c5014ae33ecee5473e7b83305fbb8410e
Ian Eaves <ian.k.eaves@gmail.com>
2022-09-12 18:56:57 -0700

    working base

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,5 +2,8 @@
 
 export GITHUB_TOKEN=$1
 export TRACKED_FILE=$2
+export GRAI_NAMESPACE=$3
+export GRAI_HOST=$4
+export GRAI_PORT=$5
 
 python3 src/main.py
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import os
 from ghapi.all import GhApi
 from dataclasses import dataclass
+from grai_source_flat_file.base import update_server
+from grai_client.endpoints.v1.client import ClientV1
 
 
 @dataclass
@@ -10,6 +12,9 @@ class config:
     repo = os.environ['GITHUB_REPOSITORY'].split('/')[-1]
     issue_number = os.environ['GITHUB_REF_NAME'].split('/')[0]
     file = os.environ['TRACKED_FILE']
+    namespace = os.environ['GRAI_NAMESPACE']
+    host = os.environ['GRAI_HOST']
+    port = os.environ['GRAI_PORT']
 
 
 def build_message():
@@ -36,7 +41,11 @@ def file_deleted():
 def main():
     message = build_message()
     if not os.path.exists(config.file):
-        
+        raise f"{config.file} does not exist"
+    
+    client = ClientV1(config.host, config.port)
+    client.set_authentication_headers(username='null@grai.io', password='super_secret')
+    update_server(client, config.file, config.namespace)
 
 
 if __name__ == "__main__":
