5beffe072d8967e269b3cad12c74856d029ee470
Ian Eaves <ian.k.eaves@gmail.com>
2022-09-16 19:55:56 -0700

    issuenumbers

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,5 +6,6 @@ export GRAI_NAMESPACE=$3
 export GRAI_HOST=$4
 export GRAI_PORT=$5
 export GRAI_AUTH_TOKEN=$6
+export PR_NUMBER=$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
 
 python3 /src/main.py
