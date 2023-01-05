4347c5a12f0e72a8df38969acb78e40ea8cbf84d
Ian Eaves <ian.k.eaves@gmail.com>
2023-01-04 19:23:36 -0600

    frontend (#4)

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -7,6 +7,7 @@ export GRAI_HOST=$4
 export GRAI_PORT=$5
 export GRAI_API_KEY=$6
 export GRAI_WORKSPACE=$7
+export GRAI_FRONTEND_HOST=$8
 export PR_NUMBER=$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
 
 python3 /src/main.py
