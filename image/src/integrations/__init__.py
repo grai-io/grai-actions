cd01fb216eefc18b91996e89144f89b3783077e5
Ian Eaves <ian.k.eaves@gmail.com>
2023-01-06 14:49:37 -0600

    imports

--- image/src/integrations/__init__.py
+++ image/src/integrations/__init__.py
@@ -3,8 +3,8 @@ import os
 
 match os.environ['GRAI_ACCESS_MODE']:
     case ['dbt']:
-        from ..dbt import get_nodes_and_edges
-        import ..dbt as integration
+        from dbt import get_nodes_and_edges
+        import dbt as integration
     case ['flat_file']:
-        from ..flat_file import get_nodes_and_edges
-        import ..flat_file as integration
+        from flat_file import get_nodes_and_edges
+        import flat_file as integration
