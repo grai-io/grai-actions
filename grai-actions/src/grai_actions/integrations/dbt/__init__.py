4ea22b6f03d75cf95eb2637daea2195c23292efb
Ian Eaves <ian.k.eaves@gmail.com>
2023-01-21 16:31:42 -0600

    removed dataclass

--- grai-actions/src/grai_actions/integrations/dbt/__init__.py
+++ grai-actions/src/grai_actions/integrations/dbt/__init__.py
@@ -1,22 +1,19 @@
-import os
-from dataclasses import dataclass
-
 from grai_source_dbt import base
+from pydantic import BaseSettings, FilePath
+
+from grai_actions.config import config
 
 
-@dataclass
-class Args:
-    manifest_file: str = os.environ["GRAI_DBT_MANIFEST_FILE"]
-    namespace: str = os.environ.get("GRAI_NAMESPACE", "default")
+class Args(BaseSettings):
+    grai_dbt_manifest_file: FilePath
 
 
 args = Args()
-assert os.path.exists(args.manifest_file), f"{args.manifest_file} does not exist"
 
 
 def get_nodes_and_edges(client):
     # Already adapted to client
     nodes, edges = base.get_nodes_and_edges(
-        args.manifest_file, args.namespace, client.id
+        args.grai_dbt_manifest_file.name, config.grai_namespace, client.id
     )
     return nodes, edges
--- grai-actions/src/grai_actions/integrations/flat_file/__init__.py
+++ grai-actions/src/grai_actions/integrations/flat_file/__init__.py
@@ -1,22 +1,21 @@
-import os
-from dataclasses import dataclass
-
 from grai_source_flat_file import base
 from grai_source_flat_file.adapters import adapt_to_client
+from pydantic import BaseSettings, FilePath
+
+from grai_actions.config import config
 
 
-@dataclass
-class Args:
-    file: str = os.environ["GRAI_TRACKED_FILE"]
-    namespace: str = os.environ.get("GRAI_NAMESPACE", "default")
+class Args(BaseSettings):
+    grai_tracked_file: FilePath
 
 
 args = Args()
-assert os.path.exists(args.file), f"{args.file} does not exist"
 
 
 def get_nodes_and_edges(client):
-    nodes, edges = base.get_nodes_and_edges(args.file, args.namespace)
+    nodes, edges = base.get_nodes_and_edges(
+        args.grai_tracked_file.name, config.grai_namespace
+    )
     nodes = adapt_to_client(nodes)
     # edges = adapt_to_client(edges)
     return nodes, []
