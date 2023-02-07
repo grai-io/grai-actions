b18d873fe2d98952e89816c511ac6cb4567b8182
Edward Louth <edward@louths.com>
2023-02-07 09:44:39 +0000

    Add action parameters to connection

--- image/grai-actions/src/grai_actions/integrations/mssql/__init__.py
+++ image/grai-actions/src/grai_actions/integrations/mssql/__init__.py
@@ -5,9 +5,32 @@ from pydantic import BaseSettings
 from grai_actions.config import config
 
 
+class Args(BaseSettings):
+    grai_mssql_host: str
+    grai_mssql_port: str
+    grai_mssql_database_name: str
+    grai_mssql_user: str
+    grai_mssql_password: str
+    grai_mssql_encrypt: bool
+    grai_mssql_trusted_connection: bool
+    grai_mssql_protocol: str
+    grai_mssql_server: str
+
+
+args = Args()
+
+
 def get_nodes_and_edges(client):
     conn = MsSQLConnector(
+        user=args.grai_mssql_user,
+        password=args.grai_mssql_password,
+        database=args.grai_mssql_database_name,
+        server=args.grai_mssql_server,
+        host=args.grai_mssql_host,
+        port=args.grai_mssql_port,
+        encrypt=args.grai_mssql_encrypt,
         namespace=config.namespace,
+        additional_connection_strings=["TrustServerCertificate=yes"] if args.grai_mssql_trusted_connection else None
     )
 
     # Already adapted to client
