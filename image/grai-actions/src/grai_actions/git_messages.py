a2f3af5ac1692eddc06c34b42e835c82da7bc0ea
Ian Eaves <ian.k.eaves@gmail.com>
2023-02-07 17:31:25 -0600

    editable comments

--- image/grai-actions/src/grai_actions/git_messages.py
+++ image/grai-actions/src/grai_actions/git_messages.py
@@ -1,3 +1,5 @@
+from typing import List, Optional
+
 from ghapi.all import GhApi
 
 from grai_actions.config import config
@@ -18,14 +20,36 @@ def heading(string, level):
     return f"<h{level}> {string} </h{level}>"
 
 
-def create_or_edit_comment(pr_number, message):
-    pass
+class BotApi:
+    def __init__(self):
+        self.api = GhApi(
+            owner=config.github_repository_owner,
+            repo=config.repo_name,
+            token=config.github_token.get_secret_value(),
+        )
+        self.bot_user = self.api.users.get_authenticated()
+        self.bot_user_id = self.bot_user["id"]
+        self.test_signal_text = "<!-- test marker text -->"
+
+    def add_comment_identifier(self, message):
+        return f"{self.test_signal_text}{message}"
+
+    def get_marked_comment(self) -> Optional[dict]:
+        current_comments = self.api.issues.list_comments(config.pr_number)
+        user_comments = (comment for comment in current_comments if comment["user"]["id"] == self.bot_user_id)
+        for comment in user_comments:
+            if self.test_signal_text in comment["body"]:
+                return comment
+        return None
+
+    def create_or_edit_comment(self, message):
+        message = self.add_comment_identifier(message)
+        if (comment := self.get_marked_comment()) is None:
+            self.api.issues.create_comment(config.pr_number, body=message)
+        else:
+            self.api.issues.update_comment(comment["id"], body=message)
 
 
 def post_comment(message):
-    api = GhApi(
-        owner=config.github_repository_owner,
-        repo=config.repo_name,
-        token=config.github_token.get_secret_value(),
-    )
-    api.issues.create_comment(config.pr_number, body=message)
+    bot = BotApi()
+    bot.create_or_edit_comment(message)
