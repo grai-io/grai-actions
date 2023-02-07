92f7366ba5613cb6187c471138ea5ca8092bfac4
Edward Louth <edward@louths.com>
2023-02-07 10:30:47 +0000

    add gnupg2

--- image/entrypoints/mssql.sh
+++ image/entrypoints/mssql.sh
@@ -2,7 +2,7 @@
 
 pip3 install grai-source-mssql
 
-apt update && apt install -y unixodbc
+apt update && apt install -y unixodbc gnupg2
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - \
     && curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list \
     && apt-get update \
