f3e1133e4c8f5cc30b5111836183c6490adf8923
Edward Louth <edward@louths.com>
2023-02-07 12:17:18 +0000

    add optional

--- image/grai-actions/src/grai_actions/integrations/mssql/__init__.py
+++ image/grai-actions/src/grai_actions/integrations/mssql/__init__.py
@@ -13,10 +13,10 @@ class Args(BaseSettings):
     grai_mssql_database: str
     grai_mssql_user: str
     grai_mssql_password: str
-    grai_mssql_encrypt: bool
-    grai_mssql_trusted_connection: bool
+    grai_mssql_encrypt: Optional[bool]
+    grai_mssql_trusted_connection: Optional[bool]
     grai_mssql_protocol: str
-    grai_mssql_server: str
+    grai_mssql_server: Optional[str]
 
     @validator("grai_mssql_encrypt", pre=True)
     def verify_encrypt(cls, value):
