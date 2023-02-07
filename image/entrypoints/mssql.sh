a5b0b4661766a3d5ef25f9f04675303c36eb042b
Ian Eaves <ian.k.eaves@gmail.com>
2023-02-07 12:03:37 -0600

    extraneous validation

--- image/entrypoints/mssql.sh
+++ image/entrypoints/mssql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip3 install grai-source-mssql2==0.0.2-alpha.6
+pip3 install grai-source-mssql2==0.0.2
 
 apt update && apt install -y unixodbc gnupg2 curl
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - \
--- image/grai-actions/src/grai_actions/integrations/mssql/__init__.py
+++ image/grai-actions/src/grai_actions/integrations/mssql/__init__.py
@@ -7,48 +7,12 @@ from pydantic import BaseSettings, validator
 from grai_actions.config import config
 
 
-class Args(BaseSettings):
-    grai_mssql_host: str
-    grai_mssql_port: Optional[str] = None
-    grai_mssql_database: str
-    grai_mssql_user: str
-    grai_mssql_password: str
-    grai_mssql_encrypt: Optional[bool] = None
-    grai_mssql_trusted_connection: Optional[bool] = None
-    grai_mssql_protocol: str
-    grai_mssql_server: Optional[str] = None
-
-    @validator("grai_mssql_encrypt", pre=True)
-    def verify_encrypt(cls, value):
-        return value == "true"
-
-    @validator("grai_mssql_trusted_connection", pre=True)
-    def verify_trusted_connection(cls, value):
-        return value == "true"
-
-    @validator("grai_mssql_server", pre=True)
-    def verify_server(cls, value):
-        print(f"server:pre: {value}")
-
-        return None if value is None or value == "none" or value == "" else value
-
-
 args = Args()
 
 
 def get_nodes_and_edges(client):
-    print(f"server: {args.grai_mssql_server}")
-    print(f"encrypt: {args.grai_mssql_encrypt}")
-    print(f"trusted_connection: {args.grai_mssql_trusted_connection}")
-
     conn = MsSQLConnector(
-        user=args.grai_mssql_user,
-        password=args.grai_mssql_password,
-        database=args.grai_mssql_database,
-        server=args.grai_mssql_server,
-        host=args.grai_mssql_host,
-        port=args.grai_mssql_port,
-        encrypt=args.grai_mssql_encrypt,
+
         namespace=config.grai_namespace,
         additional_connection_strings=["TrustServerCertificate=yes"],
     )
