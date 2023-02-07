f0c85c004cc0998ee6c8e89f6c64f8e744e06a83
Edward Louth <edward@louths.com>
2023-02-07 10:38:45 +0000

    sort out some args issues

--- image/grai-actions/src/grai_actions/integrations/mssql/__init__.py
+++ image/grai-actions/src/grai_actions/integrations/mssql/__init__.py
@@ -1,20 +1,21 @@
 from grai_source_mssql import base
 from grai_source_mssql.loader import MsSQLConnector
 from pydantic import BaseSettings
+from typing import Optional
 
 from grai_actions.config import config
 
 
 class Args(BaseSettings):
     grai_mssql_host: str
-    grai_mssql_port: str
-    grai_mssql_database_name: str
+    grai_mssql_port: Optional[str]
+    grai_mssql_database: str
     grai_mssql_user: str
     grai_mssql_password: str
-    grai_mssql_encrypt: bool
-    grai_mssql_trusted_connection: bool
-    grai_mssql_protocol: str
-    grai_mssql_server: str
+    grai_mssql_encrypt: Optional[bool]
+    grai_mssql_trusted_connection: Optional[bool]
+    grai_mssql_protocol: Optional[str]
+    grai_mssql_server: Optional[str]
 
 
 args = Args()
@@ -24,7 +25,7 @@ def get_nodes_and_edges(client):
     conn = MsSQLConnector(
         user=args.grai_mssql_user,
         password=args.grai_mssql_password,
-        database=args.grai_mssql_database_name,
+        database=args.grai_mssql_database,
         server=args.grai_mssql_server,
         host=args.grai_mssql_host,
         port=args.grai_mssql_port,
