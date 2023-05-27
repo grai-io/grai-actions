f7b119ac8d86208ff157289c660d32426193c368
Ian Eaves <ian.k.eaves@gmail.com>
2023-05-27 17:04:38 -0500

    test

--- tests/mysql/dev-database/init-db.sh
+++ tests/mysql/dev-database/init-db.sh
@@ -5,8 +5,9 @@ user="${DB_USER:-grai}"
 password="${DB_PASSWORD:-grai}"
 database="${DB_DATABASE:-grai}"
 
-for file in `find $SCRIPT_DIR -type f -name *.sql | sort`
-  do
-    echo "Executing $file"
-    mysql -u $user --password=$password -h $host --protocol=TCP $database < $file
- done
+for file in $(find $SCRIPT_DIR -type f -name *.sql | sort)
+do
+  echo "Executing $file"
+  mysql -u $user --password=$password -h $host --protocol=TCP $database < $file
+done
+
