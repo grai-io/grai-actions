1137b2bd8d605cd8ca06ad8cf68e3b33fb1fb3ef
Ian Eaves <ian.k.eaves@gmail.com>
2023-05-27 17:42:16 -0500

    databasename

--- tests/mysql/dev-database/init-db.sh
+++ tests/mysql/dev-database/init-db.sh
@@ -3,7 +3,7 @@ host="${DB_HOST:-localhost}"
 port="${DB_PORT:-3306}"
 user="${DB_USER:-grai}"
 password="${DB_PASSWORD:-grai}"
-database="${DB_DATABASE:-grai}"
+database="${DB_DATABASE_NAME:-grai}"
 
 for file in $(find $SCRIPT_DIR -type f -name '*.sql' | sort)
 do
