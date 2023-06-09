d23486c5c7de1b8592cfeb84c5a22c2c56b4d3cd
Ian Eaves <ian.k.eaves@gmail.com>
2023-06-09 13:52:18 -0500

    lint

--- document-action-inputs.py
+++ document-action-inputs.py
@@ -4,13 +4,23 @@ from tomark import Tomark
 import markdown
 
 
-SHARED_FIELDS = {'action', 'github-token', 'namespace', 'workspace', 'api-key', 'grai-app-url',
-                 'client-host', 'client-port', 'grai-user', 'grai-password'}
+SHARED_FIELDS = {
+    "action",
+    "github-token",
+    "namespace",
+    "workspace",
+    "api-key",
+    "grai-app-url",
+    "client-host",
+    "client-port",
+    "grai-user",
+    "grai-password",
+}
 
 SHARED_EXAMPLE_DEFAULTS = {
-    'namespace': 'my_apps_grai_namespace',
-    'api-key': 'my_grai_api_key',
-    'action': 'tests',
+    "namespace": "my_apps_grai_namespace",
+    "api-key": "my_grai_api_key",
+    "action": "tests",
 }
 
 
@@ -25,8 +35,10 @@ class IndentDumper(yaml.Dumper):
 class BuildDocResults:
     def __init__(self, doc_settings, ignored_fields=None):
         self.doc_settings = doc_settings
-        self.folder = self.doc_settings['folder']
-        self.ignored_fields = ignored_fields if ignored_fields is not None else SHARED_FIELDS
+        self.folder = self.doc_settings["folder"]
+        self.ignored_fields = (
+            ignored_fields if ignored_fields is not None else SHARED_FIELDS
+        )
 
         file = os.path.join(self.folder, "action.yaml")
         self.data = yaml.safe_load(open(file))
@@ -37,15 +49,15 @@ class BuildDocResults:
     @staticmethod
     def build_record(field_name, field_vals):
         record = {
-            'Field': field_name,
-            'Required': 'yes' if field_vals.get('required', False) else 'no',
-            'Default': field_vals.get('default', ''),
-            'Description': field_vals.get('description', '')
+            "Field": field_name,
+            "Required": "yes" if field_vals.get("required", False) else "no",
+            "Default": field_vals.get("default", ""),
+            "Description": field_vals.get("description", ""),
         }
-        return {k: v if v is not None else '' for k, v in record.items()}
+        return {k: v if v is not None else "" for k, v in record.items()}
 
     def build_table_data(self):
-        inputs = self.data['inputs']
+        inputs = self.data["inputs"]
 
         table_data = []
         for key, vals in inputs.items():
@@ -56,20 +68,27 @@ class BuildDocResults:
         return table_data
 
     def build_action_example(self):
-        with_args = {key: example for key, vals in self.data['inputs'].items()
-                     if (example := vals.get('example', None)) is not None}
+        with_args = {
+            key: example
+            for key, vals in self.data["inputs"].items()
+            if (example := vals.get("example", None)) is not None
+        }
         with_args = {**SHARED_EXAMPLE_DEFAULTS, **with_args}
         steps = [
             {"name": "Checkout", "uses": "actions/checkout@v3"},
-            {"name": "Run Grai Action", "uses": f"grai-io/grai-actions/{self.folder}@master", "with": with_args},
+            {
+                "name": "Run Grai Action",
+                "uses": f"grai-io/grai-actions/{self.folder}@master",
+                "with": with_args,
+            },
         ]
         base = {
             f"on": ["pull_request"],
-            "name": self.doc_settings.get('name', self.doc_settings['folder']),
+            "name": self.doc_settings.get("name", self.doc_settings["folder"]),
             "jobs": {
                 f"test_{self.doc_settings['folder']}": {
                     "runs-on": "ubuntu-latest",
-                    "steps": steps
+                    "steps": steps,
                 }
             },
         }
@@ -80,7 +99,7 @@ class BuildDocResults:
 
 
 with open("./docs/documented.yaml") as f:
-    documented = yaml.safe_load(f)['ready']
+    documented = yaml.safe_load(f)["ready"]
 
 SENTINEL_STRING = "<!-- Fields Sentinel Section -->"
 EXAMPLE_SENTINEL_STRING = "<!-- Example Sentinel Section -->"
@@ -97,11 +116,12 @@ for documentation in documented:
     result = f"\n\n{SENTINEL_STRING}\n\n".join(result)
 
     result = [obj.strip() for obj in file_string.split(EXAMPLE_SENTINEL_STRING)]
-    yaml_output = yaml.dump(docs.action_example, sort_keys=False, allow_unicode=True,
-                                 Dumper=IndentDumper)
+    yaml_output = yaml.dump(
+        docs.action_example, sort_keys=False, allow_unicode=True, Dumper=IndentDumper
+    )
 
     # For some reason pyyaml is inserting quote literals into the output of on...
-    yaml_output = yaml_output.replace("\'on\'", 'on')
+    yaml_output = yaml_output.replace("'on'", "on")
 
     result[1] = f"```yaml copy\n{yaml_output}\n```"
     result = f"\n\n{EXAMPLE_SENTINEL_STRING}\n\n".join(result)
@@ -109,5 +129,3 @@ for documentation in documented:
     if result != file_string:
         with open(os.path.join(docs.folder, "README.md"), "w") as f:
             f.write(result)
-
-
